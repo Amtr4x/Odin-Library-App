209a3b21e881b32c3f1dd8fb22fa5da63df7e9b3
leandryscsec <leandryscsec@gmail.com>
2024-07-24 21:52:20 -0400

    Add increase book list funtionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const books = [];
 const submit = document.querySelector("#submit-btn");
+const bookList = document.querySelector("#books-list");
 
 const Book = function (title, author, amountOfPages, read) {
   this.title = title;
@@ -8,16 +9,56 @@ const Book = function (title, author, amountOfPages, read) {
   this.read = Boolean(read);
 };
 
+function addLastBook() {
+  const lastBook = books[books.length - 1];
+  const newCard = document.createElement("li");
+  const cardHead = document.createElement("div");
+  const bookTitle = document.createElement("h3");
+  const deleteButton = document.createElement("button");
+  const authorName = document.createElement("p");
+  const cardFoot = document.createElement("div");
+  const bookPages = document.createElement("p");
+  const bookRead = document.createElement("p");
+
+  bookTitle.textContent = lastBook.title;
+  deleteButton.textContent = "x";
+  authorName.textContent = lastBook.author;
+  bookPages.textContent = `pages: ${lastBook.amountOfPages}`;
+  bookRead.textContent = `${lastBook.read ? "is read!" : "not read yet"}`;
+
+  cardHead.appendChild(bookTitle);
+  cardHead.appendChild(deleteButton);
+
+  cardFoot.appendChild(bookPages);
+  cardFoot.appendChild(bookRead);
+
+  newCard.appendChild(cardHead);
+  newCard.appendChild(authorName);
+  newCard.appendChild(cardFoot);
+  bookList.appendChild(newCard);
+}
+
 function retrieveBook() {
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const title = document.querySelector("#book-title").value;
-    const author = document.querySelector("#book-author").value;
-    const amountOfPages = document.querySelector("#pages").value;
-    const isRead = document.querySelector("#read").checked;
+    const title = document.querySelector("#book-title");
+    const author = document.querySelector("#book-author");
+    const amountOfPages = document.querySelector("#pages");
+    const isRead = document.querySelector("#read");
 
-    const newBook = new Book(title, author, amountOfPages, isRead);
+    const newBook = new Book(
+      title.value,
+      author.value,
+      amountOfPages.value,
+      isRead.checked,
+    );
     books.push(newBook);
+
+    title.value = "";
+    author.value = "";
+    amountOfPages.value = "";
+    isRead.checked = false;
+    addLastBook();
   });
 }
 
